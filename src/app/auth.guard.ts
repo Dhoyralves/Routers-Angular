6e4fcabaa04737ee2948666029d794eb495054df
Dhoyralves <dhoyralves@gmail.com>
2023-04-17 02:51:43 -0300

    use of canActivate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 @Injectable({ //tem um decorador de injetavel
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate { //vou ter que usar o serviço de autenticação para ver se estou logado com o token no navegador, entao se eu tiver logado vou deixaro usuario ir praquela pagina, se não tiver logado vou retornar um false no canActivate, não deixando eu acessar a pagina... começo com o construtor nessa parte
   constructor(
     private auth:AuthService,
     private router: Router
@@ -15,16 +15,17 @@ export class AuthGuard implements CanActivate {
   canActivate( //metodo
     route: ActivatedRouteSnapshot, //dependencias route e state e retorna um obserble
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    //return false; // coloque isso primeiro depois mude //aqui que ele atribue o acesso
+    //return false; // coloque isso inicialmente retornando false, depois mude //esse return atribue o acesso a pagina
     if(!this.auth.estaAutenticado()){
-      this.router.navigate(["login"]);
+      this.router.navigate(["login"]); //quando ele não tiver autenticado ele vai para pagina de login, se eu não tiver o acess-token tbm ele vem pra cá
       return false;
     }
 
-    return true;
+    return true; //se tiver feito o login, ele vai para a pagina
   }
   
 }
 
-//esse guard é para limitar o acesso a pagina, somente pra quem se autenticar
+//os routes guard é para limitar o acesso as rotas de paginas, somente pra quem se autenticar, ou proteger algumas rotas ex.: login
 //ng g guard auth (pode mudar esse nome auth) com a interface de implementação CanActivate
+// esse guard trabalha junto com o service para fazer a autenticação de login
